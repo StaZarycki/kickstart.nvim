4ea4b4f15646941eee2bec74b8de2dc61f461344
StaZarycki <stazarycki@gmail.com>
2024-05-18 12:02:09 +0200

    feat: nvim tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,4 +8,51 @@ return {
     open_mapping = [[<c-\>]],
     direction = 'float',
   } },
+
+  -- Nvim tree
+  {
+    'nvim-tree/nvim-tree.lua',
+    version = '*',
+    lazy = false,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      -- Disable netrw
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
+      -- Enable 24-bit colors
+      vim.opt.termguicolors = true
+
+      -- Custom mappings
+      local function my_on_attach(bufnr)
+        local api = require 'nvim-tree.api'
+
+        local function opts(desc)
+          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- Default mappings
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- Delete
+        vim.keymap.del('n', '<C-e>', { buffer = bufnr })
+
+        -- Set
+        vim.keymap.set('n', '<C-e>', api.tree.toggle, { desc = 'Toggle nvim tree' })
+        vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts 'Go to parent directory')
+        vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+      end
+
+      require('nvim-tree').setup {
+        on_attach = my_on_attach,
+        filters = {
+          dotfiles = false,
+        },
+      }
+
+      require('nvim-tree.api').tree.open()
+    end,
+  },
 }
